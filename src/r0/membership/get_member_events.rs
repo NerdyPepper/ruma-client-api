5a99d6739c923b7fd21a0cf8bb26239c1b8743ba
Akshay <nerdypepper@tuta.io>
2020-03-27 19:39:00 +0530

    Add query parameters to member list endpoint, fix #120

--- src/r0/membership/get_member_events.rs
+++ src/r0/membership/get_member_events.rs
@@ -3,6 +3,7 @@
 use ruma_api::ruma_api;
 use ruma_events::{room::member::MemberEvent, EventResult};
 use ruma_identifiers::RoomId;
+use serde::{Deserialize, Serialize};
 
 ruma_api! {
     metadata {
@@ -18,6 +19,25 @@ ruma_api! {
         /// The room to get the member events for.
         #[ruma_api(path)]
         pub room_id: RoomId,
+
+        /// The point in time (pagination token) to return members for in the room. This token can
+        /// be obtained from a prev_batch token returned for each room by the sync API.
+        #[serde(skip_serializing_if = "Option::is_none")]
+        #[ruma_api(query)]
+        pub at: Option<String>,
+
+        /// The kind of membership to filter for. Defaults to no filtering if unspecified. When
+        /// specified alongside not_membership, the two parameters create an 'or' condition: either
+        /// the membership is the same as membership or is not the same as not_membership.
+        #[serde(skip_serializing_if = "Option::is_none")]
+        #[ruma_api(query)]
+        pub membership: Option<MembershipFilter>,
+
+        /// The kind of memberships to *exclude* from the results. Defaults to no filtering if
+        /// unspecified.
+        #[serde(skip_serializing_if = "Option::is_none")]
+        #[ruma_api(query)]
+        pub not_membership: Option<MembershipFilter>,
     }
 
     response {
@@ -27,4 +47,24 @@ ruma_api! {
     }
 
     error: crate::Error
+}
+
+/// The kind of membership state of a user to filter for.
+#[derive(Clone, Copy, Debug, Deserialize, Serialize)]
+pub enum MembershipFilter {
+    /// The user has joined.
+    #[serde(rename = "join")]
+    Join,
+
+    /// The user has been invited.
+    #[serde(rename = "invite")]
+    Invite,
+
+    /// The user has left.
+    #[serde(rename = "leave")]
+    Leave,
+
+    /// The user has is banned.
+    #[serde(rename = "ban")]
+    Ban,
 }
